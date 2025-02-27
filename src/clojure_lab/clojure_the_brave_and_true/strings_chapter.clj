8929ee549aa749590b8ead30060fd76905a4c532
Jhonatan S. Souza <jhonatan2760@gmail.com>
2025-02-26 23:06:16 -0300

    Starting the clojure and brave true book

---
+++ src/clojure_lab/clojure_the_brave_and_true/strings_chapter.clj
@@ -0,0 +1,11 @@
+(ns clojure-lab.clojure-the-brave-and-true.strings-chapter)
+
+; Strings represent text. The name comes from the ancient Phoenicians, who
+;one day invented the alphabet after an accident involving yarn. Here are
+;some examples of string literals:
+
+(def name "Jhonatan")
+(def name-concatenation (str "Name: - " name))
+(def multiple-lines-strings "line 1 - long string content
+\\ line 2 - long string content.")
+
